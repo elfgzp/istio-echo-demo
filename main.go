0933a0c0e6d8422d9e7d4f1ce385caed65d9ab87
elfgzp <me@elfgzp.cn>
2020-05-19 15:15:45 +0800

    [ADD] add indexV2

--- main.go
+++ main.go
@@ -11,9 +11,16 @@ func index(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, os.Getenv("ECHO"))
 }
 
+func indexV2(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, os.Getenv("ECHO2"))
+}
+
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/v2", indexV2)
+
 	fmt.Println("http://127.0.0.1:8088")
     http.ListenAndServe(":8088", nil)
 }
