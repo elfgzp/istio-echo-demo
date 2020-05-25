576dcf739a489c6e2ea2943556141ef8446db1c6
elfgzp <me@elfgzp.cn>
2020-05-25 12:52:53 +0800

    [MOD] 修改 main.go

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
     "net/http"
 )
 
+func PrintRequestMiddleware(handlerfunc func(w http.ResponseWriter, req *http.Request)) func(w http.ResponseWriter, req *http.Request) {
+    return func(w http.ResponseWriter, req *http.Request)  {
+        fmt.Printf("%v\n", req)
+        handlerfunc(w, req)
+    }
+}
+
 func index(w http.ResponseWriter, req *http.Request) {
 
     fmt.Fprintf(w, os.Getenv("ECHO"))
@@ -18,8 +25,8 @@ func indexV2(w http.ResponseWriter, req *http.Request) {
 
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/v2", indexV2)
+	http.HandleFunc("/", PrintRequestMiddleware(index))
+	http.HandleFunc("/v2", PrintRequestMiddleware(indexV2))
 
 	fmt.Println("http://127.0.0.1:8088")
     http.ListenAndServe(":8088", nil)
