96c9c30ae0048444be4cc2ff77f340432a9408f8
elfgzp <me@elfgzp.cn>
2020-08-13 17:02:53 +0800

    feat: 增加 code 接口

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func PrintRequestMiddleware(handlerfunc func(w http.ResponseWriter, req *http.Request)) func(w http.ResponseWriter, req *http.Request) {
@@ -23,9 +24,17 @@ func indexV2(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, os.Getenv("ECHO2"))
 }
 
+func code(w http.ResponseWriter, req *http.Request) {
+	codeStr := req.URL.Query().Get("code")
+	code, _ := strconv.Atoi(codeStr)
+	w.WriteHeader(code)
+	w.Write([]byte(codeStr))
+}
+
 func main() {
 	http.HandleFunc("/", PrintRequestMiddleware(index))
 	http.HandleFunc("/v2", PrintRequestMiddleware(indexV2))
+	http.HandleFunc("/code", PrintRequestMiddleware(code))
 
 	fmt.Println("http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", nil)
